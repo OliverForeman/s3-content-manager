fcc08098d90ebfb1fac79fa4f2aebd7a2ff51c39
Oliver Foreman <oforeman@scottlogic.com>
2024-07-12 15:15:21 +0100

    init backend with gradle and spring

---
+++ backend/src/main/java/com/github/oliverforeman/s3contentmanager/S3contentmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.github.oliverforeman.s3contentmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3contentmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3contentmanagerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/oliverforeman/s3contentmanager/S3contentmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.oliverforeman.s3contentmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3contentmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
